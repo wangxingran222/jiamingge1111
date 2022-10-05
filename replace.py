540f9b21df4222065f6beddcbe028833b18df629
Geaming <71267655+Geaming-CHN@users.noreply.github.com>
2022-10-06 01:27:18 +0800

    Update replace.py

--- replace.py
+++ replace.py
@@ -2,7 +2,8 @@ import os
 
 path = '/home/runner/work/jiamingge1111/jiamingge1111'
 for file_name in os.listdir(path):
-    print(file_name)
+    if isfile(file_name):
+        print(file_name)
 #     with open(os.path.join(path,file_name),"r",encoding='utf-8')as f1,open(os.path.join(path,file_name+".bak"),"w",encoding='utf-8')as f2:
 #         for line in f1:
 #             if "@pytest.mark.replace" in line:
