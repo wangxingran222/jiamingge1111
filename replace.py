e0a2787ee5daf901ed2085612fb6f980fc9d2592
Geaming <71267655+Geaming-CHN@users.noreply.github.com>
2022-10-06 01:00:57 +0800

    Update replace.py

--- replace.py
+++ replace.py
@@ -1,7 +1,8 @@
 import os
 
-path = './'
+path = ''
 for file_name in os.listdir(path):
+    print(os.path.join(path,file_name))
     with open(os.path.join(path,file_name),"r",encoding='utf-8')as f1,open(os.path.join(path,file_name+".bak"),"w",encoding='utf-8')as f2:
         for line in f1:
             if "@pytest.mark.replace" in line:
