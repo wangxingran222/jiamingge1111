4a7038241502cbe5547659b8f10bfb77f22a91a3
Geaming <71267655+Geaming-CHN@users.noreply.github.com>
2022-10-06 01:21:22 +0800

    Update replace.py

--- replace.py
+++ replace.py
@@ -1,12 +1,12 @@
 import os
 
-path = ''
+path = '/home/runner/work/jiamingge1111/jiamingge1111'
 for file_name in os.listdir(path):
-    print(os.path.join(path,file_name))
-    with open(os.path.join(path,file_name),"r",encoding='utf-8')as f1,open(os.path.join(path,file_name+".bak"),"w",encoding='utf-8')as f2:
-        for line in f1:
-            if "@pytest.mark.replace" in line:
-                line = line.replace("@pytest.mark.replace","@pytest.mark.RRR")
-            f2.write(line)
-    os.remove(os.path.join(path,file_name))
-    os.rename(os.path.join(path,file_name+".bak"),os.path.join(path,file_name))
+    print(file_name)
+#     with open(os.path.join(path,file_name),"r",encoding='utf-8')as f1,open(os.path.join(path,file_name+".bak"),"w",encoding='utf-8')as f2:
+#         for line in f1:
+#             if "@pytest.mark.replace" in line:
+#                 line = line.replace("@pytest.mark.replace","@pytest.mark.RRR")
+#             f2.write(line)
+#     os.remove(os.path.join(path,file_name))
+#     os.rename(os.path.join(path,file_name+".bak"),os.path.join(path,file_name))
