5d6804c8b1d2464b4bf4cc36bd44b31427809d86
Geaming <71267655+Geaming-CHN@users.noreply.github.com>
2022-10-06 10:45:09 +0800

    Update replace.py

--- tests/replace.py
+++ tests/replace.py
@@ -4,12 +4,10 @@ from os.path import isfile
 def replace_label(path, old_label:str, new_label:str):
     for file_name in os.listdir(path):
         if isfile(os.path.join(path,file_name)):
-            print(file_name)
             with open(os.path.join(path,file_name),"r",encoding='utf-8')as f1,open(os.path.join(path,file_name+".bak"),"w",encoding='utf-8')as f2:
                 for line in f1:
                     if old_label in line:
                         line = line.replace(old_label, new_label)
-                        print(line)
                     f2.write(line)
             os.remove(os.path.join(path,file_name))
             os.rename(os.path.join(path,file_name+".bak"),os.path.join(path,file_name))
