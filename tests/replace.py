143211eb3fc1c58464625b33d107c3f2dfdfa129
Geaming <71267655+Geaming-CHN@users.noreply.github.com>
2022-10-06 23:21:18 +0800

    Update replace.py

--- tests/replace.py
+++ tests/replace.py
@@ -18,3 +18,4 @@ def replace_label(path, old_label:str, new_label:str):
 if __name__ == "__main__":
     cur_dir = os.path.dirname(os.path.abspath(__file__))  #当前文件所在目录
     replace_label(cur_dir,"@pytest.mark.RR1","@pytest.mark.R")
+    
